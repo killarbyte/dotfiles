c3a601c6650ef46860f0a37b2bf6362f055569b9
Anton K <killarbyte@gmail.com>
2020-05-15 18:31:27 +0300

    msg

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+### Dynamic Data
+currentuser=$(who | awk 'NR==1{print $1}')
+
 ### Copy Files ###
 cp .bash_aliases ~/
 cp .streamlinkrc ~/
 
 ### make Directorys ###
-mkdir -p ~/Downloads/git
+mkdir -p /home/$currentuser/Downloads/git
 
 ### enable stuff ###
-source ~/.bashrc
+source /home/$currentuser/.bashrc
