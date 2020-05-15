f58c67d4609ae7d2417247d37a30d360ed122dd9
Anton K <killarbyte@gmail.com>
2020-05-15 18:32:03 +0300

    msg

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 currentuser=$(who | awk 'NR==1{print $1}')
 
 ### Copy Files ###
-cp .bash_aliases ~/
-cp .streamlinkrc ~/
+cp .bash_aliases /home/$currentuser/
+cp .streamlinkrc /home/$currentuser/
 
 ### make Directorys ###
 mkdir -p /home/$currentuser/Downloads/git
