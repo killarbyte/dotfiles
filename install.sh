34493ba24e205a446ae74d59dd18eedbf4a054b4
Anton K <killarbyte@gmail.com>
2020-05-24 02:47:38 +0300

    msg

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ currentuser=$(who | awk 'NR==1{print $1}')
 \cp -r .streamlinkrc /home/$currentuser/
 
 ### make Directorys ###
-mkdir -p /home/$currentuser/Downloads/git
+\cp -r 
 
 
 ### install livestreamers ###
